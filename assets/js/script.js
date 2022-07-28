807ffc1889f239e716a95ed3cf528607cdf20dd6
Seb <sebastianfrancix@gmail.com>
2022-07-28 16:05:03 +0300

    added the increment button js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let count = 0;
+// grab the count-el element and store it in a countEl variable
+let countEl = document.getElementById("count-el");
+// console.log(countEl);
 
 function increment() {
   count = count + 1;
-  console.log(count);
+  //   set countEl's innerText or innerHTML to the count
+  countEl.innerText = count;
+  //   console.log(count);
 }
